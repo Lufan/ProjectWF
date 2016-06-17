83a708629439e186491d6c2dd2a14e1a0b53d05e
Lufan <sync02@gmail.com>
2016-06-17 18:52:38 +0300

    Append POCO classes for User and Role management

---
+++ web/Models/UserViewModels.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using DomainLayer.Identity;
+
+namespace PresentationLayer.Models
+{
+    public class AppUserCreateModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+
+    public class LoginModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+
+    public class RoleEditModel
+    {
+        public AppUserRole Role { get; set; }
+        public IEnumerable<AppUser> Members { get; set; }
+        public IEnumerable<AppUser> NonMembers { get; set; }
+    }
+
+    public class RoleModificationModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+        public string[] IdsToAdd { get; set; }
+        public string[] IdsToDelete { get; set; }
+    }
+}
